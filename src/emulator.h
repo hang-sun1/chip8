ac585bcd7367b85a0ec7df554a4c96b125537dc7
Hang Sun <sun.hang2000@gmail.com>
2022-06-17 13:25:52 -0500

    add program loading; add subset of instructions

--- src/emulator.h
+++ src/emulator.h
@@ -2,8 +2,30 @@
 
 #include <stdint.h>
 
+#define MEMORY_SIZE 4096
+#define PROGRAM_START_OFFSET 512
+#define MAX_PROGRAM_SIZE (MEMORY_SIZE - PROGRAM_START_OFFSET)
+#define STACK_SIZE 1024
+
+// width is 8 bytes or 64 bits
+#define DISPLAY_WIDTH 8
+// height is 4 bytes or 32 bits
+#define DISPLAY_HEIGHT 4
+
 typedef struct Emulator {
-    uint8_t memory[4096];
+    uint8_t memory[MEMORY_SIZE];
+    uint16_t stack[STACK_SIZE];
+    // sp == Stack pointer
+    uint16_t sp;
+    // program counter
     uint16_t pc;
-     
+    // The registers, named/indexed V0 - VF
+    uint8_t registers[16];
+    uint16_t index_register;
+
+    uint64_t graphics[DISPLAY_HEIGHT];
 } Emulator;
+
+void init_emulator(Emulator *emulator);
+void load_program(Emulator *emulator, uint8_t *program, long program_size);
+void run(Emulator *emulator);
--- src/main.c
+++ src/main.c
@@ -1,5 +1,45 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-    puts("Hello!");
+#include "emulator.h"
+
+int main(int argc, char **argv) {
+    Emulator emu;
+    init_emulator(&emu);
+
+    // check that the user provides a file
+    if (argc < 2) {
+        puts("Please point the emulator to a program file");
+        return EXIT_FAILURE;
+    }
+    FILE *program;
+    program = fopen(argv[1], "rb");
+    if (!program) {
+        printf("Failed to open file: %s", argv[1]);
+        goto Error;
+    }
+    fseek(program, 0, SEEK_END);
+    long file_len = ftell(program);
+    if (file_len > MAX_PROGRAM_SIZE || file_len == -1) {
+        puts("The program provided may be too large");
+        goto Error;
+    }
+    rewind(program);
+    uint8_t buffer[4096];
+    fread(buffer, file_len, 1, program);
+    fclose(program);
+
+    load_program(&emu, buffer, file_len);
+
+    do {
+        run(&emu);
+    } while (1);
+
+    return EXIT_SUCCESS;
+
+    Error:
+    if (program) {
+        fclose(program);
+    }
+    return EXIT_FAILURE;
 }
